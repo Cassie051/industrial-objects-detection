e337b889b30fe280aa281a1ed7b3b1521c418f77
kasia <kasiaczarna051@gmail.com>
2022-04-21 01:26:15 +0200

    add rand and jetbot move

--- object_detection.py
+++ object_detection.py
@@ -1,21 +1,13 @@
-# Copyright (c) 2020-2021, NVIDIA CORPORATION.  All rights reserved.
-#
-# NVIDIA CORPORATION and its licensors retain all intellectual property
-# and proprietary rights in and to this software, related documentation
-# and any modifications thereto.  Any use, reproduction, disclosure or
-# distribution of this software and related documentation without an express
-# license agreement from NVIDIA CORPORATION is strictly prohibited.
-
 from omni.isaac.examples.base_sample import BaseSample
 from omni.isaac.core.utils.nucleus import find_nucleus_server
 from omni.isaac.core.utils.stage import add_reference_to_stage
-from omni.isaac.core.robots import Robot
 from omni.isaac.jetbot import Jetbot
-import numpy as np
+from omni.isaac.motion_generation import WheelBasePoseController
+from omni.isaac.jetbot.controllers import DifferentialController
+from omni.isaac.synthetic_utils import SyntheticDataHelper
 import carb
-
-# Note: checkout the required tutorials at https://docs.omniverse.nvidia.com/app_isaacsim/app_isaacsim/overview.html
-
+import numpy as np
+import math
 
 class ObjectDetection(BaseSample):
     def __init__(self) -> None:
@@ -23,21 +15,43 @@ class ObjectDetection(BaseSample):
         return
 
     def setup_scene(self):
-        world = self.get_world()
+        self.world = self.get_world()
+        self.circle_radius = 40
         result, nucleus_server = find_nucleus_server()
         if result is False:
             carb.log_error("Could not find nucleus server with /Isaac folder")
         asset_path = nucleus_server +"/Isaac/Environments/Simple_Warehouse/warehouse.usd" # warehouse_multiple_shelves.usd
         add_reference_to_stage(usd_path=asset_path, prim_path="/World/Warehouse")
-        self._jetbot = world.scene.add(Jetbot(prim_path="/World/Fancy_Jetbot",
+        asset_path = nucleus_server +"/Isaac/Environments/Simple_Warehouse/Props/SM_PaletteA_01.usd" # warehouse_multiple_shelves.usd
+        self._pallet = add_reference_to_stage(usd_path=asset_path, prim_path="/World/Palette", position=np.array([0, 30, 0]))
+        asset_path = nucleus_server +"/Isaac/Environments/Simple_Warehouse/Props/SM_PushcartA_02.usd" # warehouse_multiple_shelves.usd
+        self._pushcart = add_reference_to_stage(usd_path=asset_path, prim_path="/World/Pushcart", position=np.array([0, 30, 0]))
+        self._jetbot = self.world.scene.add(Jetbot(prim_path="/World/jetbot",
                                         name="fancy_jetbot",
                                         position=np.array([0, 30, 0])))
         return
 
     async def setup_post_load(self):
-        self._world = self.get_world()
-        self.log_info("Num of degrees of freedom after first reset: " + str(self._jetbot.num_dof)) # prints 2
-        self.log_info("Joint Positions after first reset: " + str(self._jetbot.get_joint_positions()))
+        self.rand_object_position(self._pallet)
+        self.rand_object_position(self._pushcart)
+        self._world.add_physics_callback("sending_actions", callback_fn=self.send_robot_actions)
+        self._my_controller = WheelBasePoseController(name="cool_controller",
+                                                    open_loop_wheel_controller=DifferentialController(name="open_loop_controller"),
+                                                    is_holonomic=False)
+        return
+
+    def rand_object_position(self, object):
+        self._world.reset()
+        alpha = 2 * math.pi * np.random.rand()
+        r = 100 * math.sqrt(np.random.rand()) + 20
+        object.set_world_pose(np.array([math.sin(alpha) * r, math.cos(alpha) * r, 2.5]))
+        return
+
+    def send_robot_actions(self, step_size):
+        position, orientation = self._jetbot.get_world_pose()
+        self._jetbot.apply_wheel_actions(self._my_controller.forward(start_position=position,
+                                                                     start_orientation=orientation,
+                                                                     goal_position=np.array([280, 80], [280, 280], [80, 280], [280, 80])))
         return
 
     async def setup_pre_reset(self):
